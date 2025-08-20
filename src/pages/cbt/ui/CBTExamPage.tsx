c8d5e6f181d0b2a27fbe315a8afa472f0759edb0
seho0816 <no121l@naver.com>
2025-08-21 00:19:13 +0900

    tsx 주석 추가

--- src/pages/cbt/ui/CBTExamPage.tsx
+++ src/pages/cbt/ui/CBTExamPage.tsx
@@ -1,4 +1,3 @@
-// CBTExamPage.tsx
 import React, { useEffect, useState } from 'react';
 import { certificateApi } from '@/entities/certificate/api/certificate-api';
 import { Certificate } from '@/entities';
@@ -8,15 +7,28 @@ import { Pagination } from '@/features/cbt-pagination/ui/Pagination';
 import { useNavigate } from 'react-router-dom';
 import { certificateTags } from '@/entities/certificate/model/tags';
 
+/**certificate 모델에 tag 필드를 덧붙임*/
 type UICertificate = Certificate & { tags: string[] };
 
+/**자격증 목록을 가져와 id 기반 태그를 부여하고,
+ * 선택된 태그로 필터링하여 자격증 CBT 리스트를 보여주는 컴포넌트
+ *
+ * @component
+ */
 export const CBTExamPage: React.FC = () => {
+    /**태그가 부여된 자격증 리스트*/
     const [certificates, setCertificates] = useState<UICertificate[]>([]);
+    /**현재 선택된 태그 (기본값: 전체)*/
     const [selectedTag, setSelectedTag] = useState<string>('전체');
+    /**현재 페이지 번호*/
     const [currentPage, setCurrentPage] = useState<number>(1);
+    /**한 페이지에 보여줄 자격증 개수*/
     const itemsPerPage = 12;
     const navigate = useNavigate();
 
+    /**자격증 목록을 가져와 certificate_id을 기반으로 태그를 부여함
+     * 만약 certificate_id가 없는 자격증은 빈 배열
+     */
     useEffect(() => {
         const controller = new AbortController();
 
@@ -33,11 +45,14 @@ export const CBTExamPage: React.FC = () => {
         return () => controller.abort();
     }, []);
 
-
+    /**태그가 변경되면 페이지를 1로 리셋함*/
     useEffect(() => {
         setCurrentPage(1);
     }, [selectedTag]);
 
+    /**선택된 태그가 '전체'라면 모든 자격증을 화면에 표시함
+     * '전체'가 아니라면 선택한 태그 기준으로 자격증 리스트를 필터링하여 표시
+     */
     const filteredCertificates =
         selectedTag === '전체'
             ? certificates
@@ -50,6 +65,10 @@ export const CBTExamPage: React.FC = () => {
         currentPage * itemsPerPage
     );
 
+    /**CBT 시작 버튼 클릭 시 해당 자격증의 id와 이름을 쿼리스트링에 담아 CBTStartPage로 이동한다
+     *
+     * @param {Certificate} cert - 선택한 자격증 객체
+     */
     const handleStartClick = (cert: Certificate) => {
         const query = new URLSearchParams({
             certId: cert.certificate_id.toString(),
