0881e4d0696fca225fd2f6c99d9fbe8d5cc21729
Nea4321 <ycs2219@naver.com>
2025-11-04 11:14:05 +0900

    [ + ] axios 수정

--- src/shared/api/axios-api.ts
+++ src/shared/api/axios-api.ts
@@ -2,7 +2,6 @@ import axios from "axios"
 
 // API 인스턴스 생성
 export const axiosApi = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
