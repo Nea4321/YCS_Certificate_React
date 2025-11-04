f232c450ba124587465ecd57bddd0abdd7f32dc5
Nea4321 <ycs2219@naver.com>
2025-11-04 09:54:06 +0900

    [ + ] env 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -27,6 +27,7 @@ declare module '*.less' {
   */
 
 interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
     readonly VITE_GOOGLE_CLIENT_ID: string
     readonly VITE_GOOGLE_REDIRECT_URI: string
     readonly VITE_NAVER_CLIENT_ID: string
